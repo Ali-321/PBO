09f47a33f9313cd9d6514c60a6f02261d2083159
Ali Zaenal Abidin <80868199+Ali-321@users.noreply.github.com>
2023-03-08 07:33:12 +0700

    Tugas 1 dan 2 selesai

---
+++ week2/Tugas2/src/Lampu.java
@@ -0,0 +1,14 @@
+public class Lampu {
+    // Atribut
+    boolean nyala ;
+    
+    //Method
+    void nyalakanLampu(){
+        nyala = true;
+    }
+
+    void matikanLampu(){
+       nyala = false;
+    }
+
+}
---
+++ week2/Tugas2/src/aksiLampu.java
@@ -0,0 +1,19 @@
+public class aksiLampu {
+    public static void main(String[] args) {
+      // membuat objek 
+        Lampu lampuTidur = new Lampu();
+
+        System.out.println("====");
+
+        // menyalakan lampu
+        lampuTidur.nyalakanLampu();
+        System.out.println("Apakah lampu menyala? "+ lampuTidur.nyala);
+
+        // mematikan lampu
+        lampuTidur.matikanLampu();
+        System.out.println("Apakah lampu menyala? "+ lampuTidur.nyala);
+
+        System.out.println("=====");
+        // test
+    }
+}
